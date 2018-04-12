c08e3dad4adb5b072a38535efdd283c77322d53f
Yungil Hong <hongyi0220@gmail.com>
2018-04-11 19:22:13 -0500

    Create image-overlay

--- src/components/Wall/WallPage.js
+++ src/components/Wall/WallPage.js
@@ -3,59 +3,28 @@ import PropTypes from 'prop-types';
 
 class WallPage extends React.Component {
     state = {
-        // apiKey: null,
-        // input: '',
-        // images: null,
-        // page: 1
+        pin: null
     }
 
-    // searchImage = e => {
-    //     const { input, page, iamges } = this.state;
-    //     console.log('keyDown:',e.key);
-    //
-    //     const q = input.trim().replace(/\s/g, '%20');
-    //     if (e.key === 'Enter') {
-    //         fetch(`http://localhost:3000/images?q=${q}&page=${page}`)
-    //         .then(res => res.json())
-    //         .then(resJson => {
-    //             this.setState({ images: resJson }, () => console.log(this.state))
-    //         })
-    //         .catch(err => console.log(err));
-    //     }
-    // }
-    // getKeys = () => {
-    //     return fetch('http://localhost:3000/api_key')
-    //            .then(res => res.json())
-    //            .catch(err => console.log(err));
-    // }
-    // handleInput = e => this.setState({ input: e.target.value });
-    // componentWillMount() {
-    //     this.getKeys()
-    //         .then(apiKey => this.setState({ apiKey }, () => console.log(this.state)))
-    //         .catch(err => console.log(err));
-    // }
+    highlightPin = e => {
+        if (e) console.log(e.target.id);
+        this.setState({ pin: e ? e.target.id : null });
+    }
+
     render() {
-        // const { account } = this.props;
         const { images } = this.props;
-        console.log('images:', images);
+        const { pin } = this.state;
+
         return (
             <div className='wall-page-container'>
-                {/* <div className="header">
-                    <img src="./images/pinterest_logo.png"/>
-                    <input type="text" placeholder='Search' onKeyDown={this.searchImage} onChange={this.handleInput} value={input}/>
-                    <div className="home">Home</div>
-                    <div className="user">
-                        {account.user.email.split('@')[0]}
-                    </div>
-                    <div className="options">
-                        <svg>
-                            {[...'ccc'].map((c, i) => <circle key={i} cx={12 + (i * 8)}/>)}
-                        </svg>
-                    </div>
-                </div> */}
                 <div className="wall">
                     {images ?
-                        images.map((img, i) => <div key={i} className='img-wrapper'><img src={img.src} /></div>) : ''
+                        images.map((img, i) => <div id={`pin-${i}`} key={i} className='img-container' onMouseEnter={e=>{console.log('entering'); this.highlightPin(e)}} onMouseLeave={e=>{console.log('leaving'); this.highlightPin(null)}}>
+                            <div id={`pin-${i}`} className={pin === `pin-${i}` ? 'img-overlay on': 'img-overlay'}>
+
+                            </div>
+                            <img src={img.src} />
+                        </div>) : ''
                     }
                 </div>
             </div>

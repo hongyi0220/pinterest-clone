0757dcf1a85fc615b2607f791d151c675201f5b3
Yungil Hong <hongyi0220@gmail.com>
2018-05-04 17:56:48 -0500

    Write README

---
+++ src/components/Pin/PinPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class PinPage extends React.Component {
+  render() {
+    return (
+      <div className="pin-page-container">
+        <div className="header-container">
+          <div className="share-button"></div>
+          <div className="save-button"></div>
+        </div>
+        <div className="pin-container">
+          
+        </div>
+      </div>
+    );
+  }
+}

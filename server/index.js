32d5231a545e1eaa738ecf017458a29ac20c85eb
Yungil Hong <hongyi0220@gmail.com>
2018-06-06 19:03:10 -0500

    Configure server port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 require('dotenv').config();
 const dbUrl = process.env.MONGOLAB_URI;
-const port = 3000;
+const port = process.env.PORT || 3000;
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const MongoStore = require('connect-mongo')(session);
